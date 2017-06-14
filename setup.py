1d69d483d7b3abfc99389f8d7aec24587cb1dc0d
kristofarkas <kristofarkas@gmail.com>
2017-06-14 12:08:58 +0100

    Update requirments for PyPi

--- setup.py
+++ setup.py
@@ -2,4 +2,5 @@ from setuptools import setup
 
 setup(name="ties_hybrid_topology_creator",
       version=0.1,
-      description="Ties-esc hybrid topology creator.")
+      description="Ties-esc hybrid topology creator.",
+      requires=['rdkit', 'sasmol'])
